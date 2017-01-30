ed65a73a670e2c6dca91f530aedb658f30498bc2
Jesse Roe <roeje@mail.gvsu.edu>
2017-01-29 19:33:07 -0500

    add yo generated basic MEAN app

---
+++ cognitiveApp/README.md
@@ -0,0 +1,2 @@
+# Cognitive-911-Bot
+Winter 2017 GVSU CIS Capstone project for Motorola cognitive 911 call-taking bot
---
+++ cognitiveApp/client/dev/app.js
@@ -0,0 +1,9 @@
+;(function(ng) {
+  'use strict';
+
+  ng.module('cognitiveApp', [
+      'ngResource',
+      'ngRoute',
+      'ngMessages'
+    ]);
+}(window.angular));
