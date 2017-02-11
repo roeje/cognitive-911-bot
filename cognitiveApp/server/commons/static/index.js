7558f0eea06807f8f5abcafc6c46c6da6c09d949
Jesse Roe <jessedroe@gmail.com>
2017-02-10 19:04:49 -0500

    minor changes to index page

--- cognitiveApp/server/commons/static/index.js
+++ cognitiveApp/server/commons/static/index.js
@@ -6,6 +6,7 @@ module.exports = class StaticDispatcher {
     static sendIndex(req, res) {
       var _root = process.cwd();
 
+
       res.type('.html');
 
       fs.createReadStream(_root + '/client/dev/index.html')
