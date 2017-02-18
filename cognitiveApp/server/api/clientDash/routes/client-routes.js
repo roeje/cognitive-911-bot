f31734df5df6e7b55213acad7a4b43b4ca3ac665
Jesse Roe <roeje@mail.gvsu.edu>
2017-02-18 01:01:00 -0500

    more customization to node server, create api endpoints for various actions

--- cognitiveApp/server/api/clientDash/routes/client-routes.js
+++ cognitiveApp/server/api/clientDash/routes/client-routes.js
@@ -1,16 +1,27 @@
 "use strict";
 
-const TodoController = require('../controller/client-controller');
+const ClientController = require('../controller/client-controller');
 
 module.exports = class ClientRoutes {
     static init(router) {
       router
-        .route('/api/todos')
-        .get(TodoController.getAll)
-        .post(TodoController.createTodo);
+        .route('/api/active-calls/:id')
+        .get(ClientController.getActiveCallers)
+        .delete(ClientController.deleteActiveCaller);
+
 
       router
-        .route('/api/todos/:id')
-        .delete(TodoController.deleteTodo);
+        .route('/api/call-details/:id')
+        .get(ClientController.getCallDetails)
+        .delete(ClientController.deleteCallDetails);
+
+      router
+         .route('/api/send-message/:message/:id')
+         .post()
+
+
+
     }
+
+
 }
--- cognitiveApp/server/routes/index.js
+++ cognitiveApp/server/routes/index.js
@@ -1,18 +1,18 @@
 "use strict";
 
-const TodoRoutes = require('../api/todo/routes/todo-routes');
+const ClientRoutes = require('../api/todo/routes/client-routes');
 
 const StaticDispatcher = require('../commons/static/index');
 
 
 module.exports = class Routes {
    static init(app, router) {
-     TodoRoutes.init(router);
-     
+     ClientRoutes.init(router);
+
      router
        .route('*')
        .get(StaticDispatcher.sendIndex);
-     
+
 
      app.use('/', router);
    }
