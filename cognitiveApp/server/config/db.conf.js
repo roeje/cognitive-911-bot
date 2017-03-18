7100a5cade59964a88ef0fd3430980abf0ca0e60
Jesse Roe <jessedroe@gmail.com>
2017-03-17 20:01:38 -0400

    update db.conf.js with new Mongo url

--- cognitiveApp/server/config/db.conf.js
+++ cognitiveApp/server/config/db.conf.js
@@ -8,7 +8,8 @@ module.exports = class DBConfig {
       const URL = (process.env.NODE_ENV === 'production') ? process.env.MONGOHQ_URL
                                                           : dbConst.localhost;
 
-      mongoose.connect(URL);
+      const URL_NEW = 'mongodb://remoteUser:motorolapassword@ec2-54-242-20-44.compute-1.amazonaws.com:27017/cognitiveApp';
+      mongoose.connect(URL_NEW);
       mongoose.connection.on('error', console.error.bind(console, 'An error ocurred with the DB connection: '));
     }
 };
