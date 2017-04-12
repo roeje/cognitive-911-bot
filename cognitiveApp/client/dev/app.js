01a495f7c8ecce48c1276eae946fa9cb13715dc7
Jesse Roe <jessedroe@gmail.com>
2017-04-12 03:11:14 -0400

    update transcript with line breaks

--- cognitiveApp/client/dev/app.js
+++ cognitiveApp/client/dev/app.js
@@ -1,5 +1,12 @@
 ;(function(ng) {
   'use strict';
 
-  ng.module('cognitiveApp', ['ngResource', 'ngRoute', 'ngMessages', 'underscore']);
+  ng.module('cognitiveApp', ['ngResource', 'ngRoute', 'ngMessages', 'underscore'])
+
+     .filter("trust", ['$sce', function($sce) {
+       return function(htmlCode){
+       return $sce.trustAsHtml(htmlCode);
+   }}]);
+
+
 }(window.angular));
